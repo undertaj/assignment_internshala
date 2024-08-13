427a580e75487c8953d99a5ae7d8e43a54f0d9fd
Lisha Singh <hellohellohellow16@gmail.com>
2024-08-13 12:06:58 +0530

    fix

--- lib/features/filters/view/filter_screen.dart
+++ lib/features/filters/view/filter_screen.dart
@@ -74,99 +74,97 @@ class FilterScreen extends ConsumerWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(left: 20.0, right: 20, bottom: 20, top: 5),
-              child: Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
 
-                    for(int i = 0; i < 2; i++)
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(i == 0 ? 'PROFILE' : 'CITY', style: TextStyle(color: ColorConstants.greyColor.withOpacity(0.7), fontWeight: FontWeight.w500, fontSize: 11),),
-                        const SizedBox(height: 5,),
-                        Container(
-                          height: (i == 1 ? selectedCityWatcher.isNotEmpty : selectedProfileWatcher.isNotEmpty) ? 40 : 0,
-                          child: Visibility(
-                            visible: i == 1 ? selectedCityWatcher.isNotEmpty : selectedProfileWatcher.isNotEmpty,
-                            // maintainState: true,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: i == 1 ? ref.read(cityFilterProvider.notifier).state.length : ref.read(profileFilterProvider.notifier).state.length,
-                              itemBuilder: (context, index) => Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                                child: Chip(
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-                                  backgroundColor: ColorConstants.primaryColor,
-                                  deleteIcon: const Icon(Icons.close, color: Colors.white, size: 16,),
-                                  labelStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
-                                  label: Text(i == 1 ? ref.read(cityFilterProvider.notifier).state[index] : ref.read(profileFilterProvider.notifier).state[index]),
-                                  onDeleted: () {
-                                    List<String> list;
-                                    if(i == 1) {
-                                      list = List<String>.from(ref.read(cityFilterProvider.notifier).state);
-                                    }
-                                    else {
-                                      list = List<String>.from(ref.read(profileFilterProvider.notifier).state);
-                                    }
-                                    list.removeAt(index);
-                                    if(i == 1) {
-                                      ref.read(cityFilterProvider.notifier).state = list;
-                                    }
-                                    else {
-                                      ref.read(profileFilterProvider.notifier).state = list;
-                                    }
-                                  },
-                                ),
+                  for(int i = 0; i < 2; i++)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(i == 0 ? 'PROFILE' : 'CITY', style: TextStyle(color: ColorConstants.greyColor.withOpacity(0.7), fontWeight: FontWeight.w500, fontSize: 11),),
+                      const SizedBox(height: 5,),
+                      Container(
+                        height: (i == 1 ? selectedCityWatcher.isNotEmpty : selectedProfileWatcher.isNotEmpty) ? 40 : 0,
+                        child: Visibility(
+                          visible: i == 1 ? selectedCityWatcher.isNotEmpty : selectedProfileWatcher.isNotEmpty,
+                          // maintainState: true,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: i == 1 ? ref.read(cityFilterProvider.notifier).state.length : ref.read(profileFilterProvider.notifier).state.length,
+                            itemBuilder: (context, index) => Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                              child: Chip(
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                                backgroundColor: ColorConstants.primaryColor,
+                                deleteIcon: const Icon(Icons.close, color: Colors.white, size: 16,),
+                                labelStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
+                                label: Text(i == 1 ? ref.read(cityFilterProvider.notifier).state[index] : ref.read(profileFilterProvider.notifier).state[index]),
+                                onDeleted: () {
+                                  List<String> list;
+                                  if(i == 1) {
+                                    list = List<String>.from(ref.read(cityFilterProvider.notifier).state);
+                                  }
+                                  else {
+                                    list = List<String>.from(ref.read(profileFilterProvider.notifier).state);
+                                  }
+                                  list.removeAt(index);
+                                  if(i == 1) {
+                                    ref.read(cityFilterProvider.notifier).state = list;
+                                  }
+                                  else {
+                                    ref.read(profileFilterProvider.notifier).state = list;
+                                  }
+                                },
                               ),
                             ),
                           ),
                         ),
-                        const SizedBox(height: 5,),
-                        GestureDetector(
-                          onTap: () {
-                            context.goNamed(
-                                RouteConstants.subFilterScreenName,
-                                extra: i == 0 ? 'profile' : 'city'
-                            );
-                          },
-                          child: Row(
-                            children: [
-                              const Icon(Icons.add, color: Colors.blue,size: 15,),
-                              const SizedBox(width: 9,),
-                              Text('Add ${i == 0 ? 'profile' : 'city'}', style: const TextStyle(color: ColorConstants.primaryColor, fontWeight: FontWeight.w500),)
-                            ],
-                          ),
+                      ),
+                      const SizedBox(height: 5,),
+                      GestureDetector(
+                        onTap: () {
+                          context.goNamed(
+                              RouteConstants.subFilterScreenName,
+                              extra: i == 0 ? 'profile' : 'city'
+                          );
+                        },
+                        child: Row(
+                          children: [
+                            const Icon(Icons.add, color: Colors.blue,size: 15,),
+                            const SizedBox(width: 9,),
+                            Text('Add ${i == 0 ? 'profile' : 'city'}', style: const TextStyle(color: ColorConstants.primaryColor, fontWeight: FontWeight.w500),)
+                          ],
                         ),
-                        const SizedBox(height: 22,),
+                      ),
+                      const SizedBox(height: 22,),
+                    ]
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('MAXIMUM DURATION (IN MONTHS)', style: TextStyle(color: ColorConstants.greyColor.withOpacity(0.7), fontWeight: FontWeight.w500, fontSize: 11),),
+                        const SizedBox(height: 12,),
+                        DropdownMenu(
+                          expandedInsets: EdgeInsets.zero,
+                          onSelected: (value) {
+                            ref.read(maxDurationFilterProvider.notifier).state = value as int;
+                          },
+                            initialSelection: ref.read(maxDurationFilterProvider),
+                            dropdownMenuEntries: const <DropdownMenuEntry>[
+                              DropdownMenuEntry(value: 1, label: '1',),
+                              DropdownMenuEntry(value: 2, label: '2',),
+                              DropdownMenuEntry(value: 3, label: '3',),
+                              DropdownMenuEntry(value: 4, label: '4',),
+                              DropdownMenuEntry(value: 6, label: '6',),
+                              DropdownMenuEntry(value: 12, label: '12',),
+                              DropdownMenuEntry(value: 24, label: '24',),
+                              DropdownMenuEntry(value: 36, label: '36',),
+                            ]
+                        ),
                       ]
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('MAXIMUM DURATION (IN MONTHS)', style: TextStyle(color: ColorConstants.greyColor.withOpacity(0.7), fontWeight: FontWeight.w500, fontSize: 11),),
-                          const SizedBox(height: 12,),
-                          DropdownMenu(
-                            expandedInsets: EdgeInsets.zero,
-                            onSelected: (value) {
-                              ref.read(maxDurationFilterProvider.notifier).state = value as int;
-                            },
-                              initialSelection: ref.read(maxDurationFilterProvider),
-                              dropdownMenuEntries: const <DropdownMenuEntry>[
-                                DropdownMenuEntry(value: 1, label: '1',),
-                                DropdownMenuEntry(value: 2, label: '2',),
-                                DropdownMenuEntry(value: 3, label: '3',),
-                                DropdownMenuEntry(value: 4, label: '4',),
-                                DropdownMenuEntry(value: 6, label: '6',),
-                                DropdownMenuEntry(value: 12, label: '12',),
-                                DropdownMenuEntry(value: 24, label: '24',),
-                                DropdownMenuEntry(value: 36, label: '36',),
-                              ]
-                          ),
-                        ]
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
